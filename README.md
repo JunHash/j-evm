0925c954c534c3915c7aef4b13b36a15fa48f747
Jun <junhash@163.com>
2020-03-27 01:55:33 +0800

    j-evm

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# j-evm
+## j-evm 执行
+#### 编译运行
+执行以下的几步操作即可：
+Copy至gopath目录
+*/Users/junsong/mywork/golang/src/evm
+执行代码：
+*go run *.go
+或者编译执行
+*go build .
+*./demo
+
+
---
+++ demo/chainhandler.go
@@ -0,0 +1,10 @@
+package main
+
+import "evm/kernal"
+
+type ETHChainHandler struct{}
+
+func (ethChainHandler *ETHChainHandler) GetBlockHeaderHash(uint64) kernal.Hash {
+	//just return a fake value
+	return kernal.HexToHash("this is a demo")
+}
